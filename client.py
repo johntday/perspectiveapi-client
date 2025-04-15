2c85a98629473c98cf280083e6767c0a01e3cc7d
John Day <johntday@gmail.com>
2025-04-14 19:05:30 -0500

    Add untracked files

---
+++ client.py
@@ -0,0 +1,22 @@
+from googleapiclient import discovery
+import json
+import os
+
+PERSPECTIVE_API_KEY = os.getenv('PERSPECTIVE_API_KEY')
+
+client = discovery.build(
+  "commentanalyzer",
+  "v1alpha1",
+  developerKey=PERSPECTIVE_API_KEY,
+  discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+  static_discovery=False,
+)
+
+analyze_request = {
+  'comment': { 'text': 'friendly greetings from python' },
+  'requestedAttributes': {'TOXICITY': {}}
+}
+
+response = client.comments().analyze(body=analyze_request).execute()
+print(json.dumps(response, indent=4))
+
